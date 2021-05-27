1b784fd9d45cf35784c0820dd5616d2ce547f257
Lobana-sky <loubanaeng@gmail.com>
2021-05-27 16:47:54 +0300

    update

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,10 +1,4 @@
-#!/usr/bin/env bash
 
-[ -z "$CONTRACT" ] && echo "Missing \$CONTRACT environment variable"
-[ -z "$OWNER" ] && echo "Missing \$OWNER environment variable"
-
-echo "deleting $CONTRACT and setting $OWNER as beneficiary"
-echo
 near delete $CONTRACT $OWNER
 
 echo --------------------------------------------
@@ -13,7 +7,6 @@ echo "cleaning up the /neardev folder"
 echo
 rm -rf ./neardev
 
-# exit on first error after this point to avoid redeploying with successful build
 set -e
 
 echo --------------------------------------------
@@ -27,12 +20,6 @@ echo
 echo "redeploying the contract"
 echo
 near dev-deploy ./build/release/sample.wasm
-
-echo --------------------------------------------
-echo run the following commands
-echo
-echo 'export CONTRACT=__new_contract_account_id__'
-echo
 echo
 
 exit 0
--- scripts/view_call.sh
+++ scripts/view_call.sh
@@ -1,23 +1,15 @@
-#!/usr/bin/env bash
 set -e
 
-echo
-echo \$CONTRACT is $CONTRACT
-echo \$OWNER is $OWNER
-echo
-
+# ------------------------
 near view $CONTRACT sayHi
 
+# ------------------------
 near call $CONTRACT greetingUser --account_id $OWNER
 
 # ------------------------
-
-# ------------------------
-
 near call $CONTRACT addToMyList '{"task":"new task"}' --account_id $OWNER
 near call $CONTRACT deleteTask '{"task":1}' --account_id $OWNER
 
+# ------------------------
 near call $CONTRACT showMyTasks --account_id $OWNER
 near call $CONTRACT getNumTasks --account_id $OWNER
-
-# ------------------------
