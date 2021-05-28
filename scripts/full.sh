274fb523941e794979236a706cacedae50416fd1
Lobana-sky <loubanaeng@gmail.com>
2021-05-28 17:39:08 +0300

    update

--- scripts/full.sh
+++ scripts/full.sh
@@ -1,23 +1,23 @@
-near view dev-1622208146827-4570153 sayHi
+near view dev-1622208363951-5944258 sayHi
 
-near call dev-1622208146827-4570153 greetingUser --account_id lobana.testnet 
+near call dev-1622208363951-5944258 greetingUser --account_id lobana.testnet 
 
-near call dev-1622208146827-4570153 showMyTasks --account_id lobana.testnet 
+near call dev-1622208363951-5944258 showMyTasks --account_id lobana.testnet 
 
-near call dev-1622208146827-4570153 addToMyList '{"task":""}' --account_id lobana.testnet
+near call dev-1622208363951-5944258 addToMyList '{"task":""}' --account_id lobana.testnet
 
-near call dev-1622208146827-4570153 addToMyList '{"task":"wake up"}' --account_id lobana.testnet
+near call dev-1622208363951-5944258 addToMyList '{"task":"wake up"}' --account_id lobana.testnet
 
-near call dev-1622208146827-4570153 addToMyList '{"task":"at 7 am"}' --account_id lobana.testnet
+near call dev-1622208363951-5944258 addToMyList '{"task":"at 7 am"}' --account_id lobana.testnet
 
-near call dev-1622208146827-4570153 addToMyList '{"task":"Go For shopping"}' --account_id lobana.testnet
+near call dev-1622208363951-5944258 addToMyList '{"task":"Go For shopping"}' --account_id lobana.testnet
 
-near call dev-1622208146827-4570153 addToMyList '{"task":"coding"}' --account_id lobana.testnet
+near call dev-1622208363951-5944258 addToMyList '{"task":"coding"}' --account_id lobana.testnet
 
-near call dev-1622208146827-4570153 showMyTasks --account_id lobana.testnet 
+near call dev-1622208363951-5944258 showMyTasks --account_id lobana.testnet 
 
-near call dev-1622208146827-4570153 deleteTask '{"task":1}' --account_id lobana.testnet
+near call dev-1622208363951-5944258 deleteTask '{"task":1}' --account_id lobana.testnet
 
-near call dev-1622208146827-4570153 showMyTasks --account_id lobana.testnet 
+near call dev-1622208363951-5944258 showMyTasks --account_id lobana.testnet 
 
-near call dev-1622208146827-4570153 getNumTasks --account_id lobana.testnet 
+near call dev-1622208363951-5944258 getNumTasks --account_id lobana.testnet 
